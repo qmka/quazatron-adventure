04c434af8021d10b7da776040676e9974894feb2
Michael Solovyev <eidolonzx@gmail.com>
2019-02-01 17:11:29 +0400

    Added some logics

--- game.js
+++ game.js
@@ -14,6 +14,13 @@ const gameController = (function () {
         // СЮДА НАДО ВСТАВИТЬ ПРОВЕРКИ ДЛЯ УНИКАЛЬНЫХ ЛОКАЦИЙ
         ////////////////////////////////////////////////////////
 
+        // Если лестница прислонена к дереву
+        if (gamedata.flags.isLadderLeanToTree && gamedata.currentLoc === 8) description += "<br>К дереву приставлена лестница, по которой я могу залезть наверх.";
+
+        ////////////////////////////////////////////////////////
+        // КОНЕЦ ПРОВЕРКИ УНИКАЛЬНЫХ ЛОКАЦИЙ
+        ////////////////////////////////////////////////////////
+
         description += "<br>";
         if (gamedata.items.some(e => {
                 return e.place === gamedata.currentLoc;
@@ -96,6 +103,7 @@ const wordController = (function () {
                     }
                     break;
                 case "вверх":
+                case "наверх":
                     if (gamedata.locations[gamedata.currentLoc].isU !== -1) {
                         gamedata.currentLoc = gamedata.locations[gamedata.currentLoc].isU;
                     } else {
@@ -134,12 +142,29 @@ const wordController = (function () {
                     // обрабатываем ввод игрока и производим соответствующее действие
                     // если это не прокатывает, то выводим сообщение "Я не могу этого сделать"
                     const phrase = phraseAnalyze(input);
+                    const thisItem = gamedata.items.find(e => e.case === phrase.obj);
 
                     switch (phrase.verb) {
                         case "возьми":
                         case "бери":
+                        case "забери":
                             // берём предмет
-                            const thisItem = gamedata.items.find(e => e.case === phrase.obj);
+
+                            // обработка уникальных для игры событий
+
+                            // если ты в локации с деревом, и к дереву прислонена лестница, то её можешь забрать
+                            if (thisItem.name === "лестница" && gamedata.currentLoc === 8 && gamedata.flags.isLadderLeanToTree) {
+                                // добавляем лестницу в инвентарь
+                                gamedata.items[thisItem.index].place = 999;
+                                // меняем флаг isLadderLeanToTree на false
+                                gamedata.flags.isLadderLeanToTree = false;
+                                // закрываем проход вверх
+                                gamedata.locations[8].isU = -1;
+                                output.action = `Я забрал лестницу.`;
+                                break;
+                            }
+
+                            // конец обработки уникальных для игры событий
                             if (thisItem === undefined) {
                                 output.action = `Что взять? Уточните.`;
                             } else if (thisItem.place === gamedata.currentLoc) {
@@ -155,13 +180,12 @@ const wordController = (function () {
                         case "оставь":
                         case "выбрось":
                             // кладём предмет
-                            const dropItem = gamedata.items.find(e => e.case === phrase.obj);
-                            if (dropItem === undefined) {
+                            if (thisItem === undefined) {
                                 output.action = `Что положить? Уточните.`;
-                            } else if (dropItem.place === 999) {
-                                gamedata.items[dropItem.index].place = gamedata.currentLoc;
-                                output.action = `Я положил ${dropItem.case}.`;
-                            } else if (dropItem.place === gamedata.currentLoc) {
+                            } else if (thisItem.place === 999) {
+                                gamedata.items[thisItem.index].place = gamedata.currentLoc;
+                                output.action = `Я положил ${thisItem.case}.`;
+                            } else if (thisItem.place === gamedata.currentLoc) {
                                 output.action = `Это и так здесь уже лежит!`;
                             } else {
                                 output.action = `Не могу выбросить, потому что у меня этого нет.`;
@@ -169,16 +193,80 @@ const wordController = (function () {
                             break;
                         case "осмотри":
                         case "изучи":
-                            // осматриваем предмет
-                            const examItem = gamedata.items.find(e => e.case === phrase.obj);
-                            if (examItem === undefined) {
+                            // осматриваем
+
+                            // уникальные ситуации
+                            if (phrase.obj === "пропасть" && gamedata.currentLoc === 6) {
+                                output.action = `Узкая, но глубокая пропасть. Через неё можно перейти по верёвке, но перед этим озаботьтесь тем, чтобы суметь удержать равновесие.`;
+                                break;
+                            }
+                            // общие ситуации
+                            if (thisItem === undefined) {
                                 output.action = `Ничего необычного.`;
-                            } else if (examItem.place === 999) {
-                                output.action = examItem.desc;
+                            } else if (thisItem.place === 999) {
+                                output.action = thisItem.desc;
                             } else {
                                 output.action = `У меня нет этого, так что и осматривать нечего.`;
                             }
                             break;
+
+                            // Здесь прописываем ситуации, соответствующие конкретной игре
+                        case "прислони":
+                        case "приставь":
+                            // если ты в локации с деревом, и у тебя есть лестница, то ты её можешь прислонить к дереву
+                            if (thisItem.name === "лестница" && thisItem.place === 999 && gamedata.currentLoc === 8) {
+                                // удаляем лестницу из инвентаря
+                                gamedata.items[thisItem.index].place = -1;
+                                // меняем флаг isLadderLeanToTree на true
+                                gamedata.flags.isLadderLeanToTree = true;
+                                // открываем проход наверх
+                                gamedata.locations[8].isU = 9;
+                                output.action = `Я прислонил лестницу к дереву.`;
+                            } else if (thisItem.place === 999) {
+                                output.action = `Хм, это делу не поможет.`;
+                            } else {
+                                output.action = `Я могу прислонить только то, что у меня есть.`;
+                            }
+                            break;
+                        case "сломай":
+                        case "разломай":
+                        case "ломай":
+                            // если у тебя есть лестница, то её можно разломать
+                            if (thisItem.name === "лестница" && thisItem.place === 999) {
+                                // удаляем лестницу из инвентаря
+                                gamedata.items[thisItem.index].place = -1;
+                                // добавляем в инвентарь шест
+                                gamedata.items[3].place = 999;
+                                output.action = `Я разломал лестницу, так что теперь у меня есть шест.`;
+                            } else if (thisItem.place === 999) {
+                                output.action = `Не буду ломать. Вдруг мне это ещё пригодится?`;
+                            } else {
+                                output.action = `Я не могу это сломать.`;
+                            }
+                            break;
+                        case "перейди":
+                        case "пересеки":
+                            // если у тебя есть шест, ты можешь пересечь пропасть
+                            if (phrase.obj === "пропасть" && gamedata.currentLoc === 6) {
+                                if (gamedata.items[3].place === 999) {
+                                    output.action = "Балансируя с помощью шеста, я пересёк расщелину по верёвке.";
+                                    gamedata.currentLoc = 12;
+                                } else {
+                                    output.action = "Я упаду с верёвки, мне нужно что-то для балланса.";
+                                }
+                            }
+                            else if (phrase.obj === "пропасть" && gamedata.currentLoc === 12) {
+                                if (gamedata.items[3].place === 999) {
+                                    output.action = "Балансируя с помощью шеста, я пересёк расщелину по верёвке.";
+                                    gamedata.currentLoc = 6;
+                                } else {
+                                    output.action = "Я упаду с верёвки, мне нужно что-то для балланса.";
+                                }
+                            } else {
+                                output.action = "Я не могу это сделать."
+                            }
+                            break;
+
                         default:
                             output.action = "Я не могу этого сделать";
                             break;
@@ -281,7 +369,7 @@ const controller = (function (gameCtrl, wordCtrl) {
             isD: -1
         }, {
             index: 6,
-            desc: "Вы стоите на краю страшной, бездонной пропасти. Туго натянутая верёвка пересекает расщелину, но она выглядит опасной. На север от пропасти ведёт тропинка.",
+            desc: "Вы стоите на краю страшной, бездонной пропасти. Туго натянутая верёвка пересекает расщелину. По ней можно перейти пропасть, но она выглядит слишком тонкой и опасной. На север от пропасти ведёт тропинка.",
             isN: 5,
             isE: -1,
             isS: -1,
@@ -324,6 +412,24 @@ const controller = (function (gameCtrl, wordCtrl) {
             isW: 7,
             isU: -1,
             isD: -1
+        }, {
+            index: 11,
+            desc: "Вы снаружи зловеще выглядящего замка Камелот. Величественное ранее знамя теперь изорвано в клочья и истрёпано ветрами. Большая дверь из слоновой кости на востоке - единственный вход.",
+            isN: -1,
+            isE: -1,
+            isS: -1,
+            isW: 11,
+            isU: -1,
+            isD: -1
+        }, {
+            index: 12,
+            desc: "Вы на краю Бирвудского леса. На севере верёвка пересекает ужасное ущелье. На юг от пропасти ведёт дорога.",
+            isN: -1,
+            isE: -1,
+            isS: 13,
+            isW: -1,
+            isU: -1,
+            isD: -1
         }],
 
         // Предметы
@@ -339,11 +445,23 @@ const controller = (function (gameCtrl, wordCtrl) {
             case: "рыбу",
             desc: "Это красная рыба, уже подгнившая и довольно вонючая. Такую в руках держать неприятно, а с собой таскать - так вообще мерзко.",
             place: 3
+        }, {
+            index: 2,
+            name: "меч",
+            case: "меч",
+            desc: "Это короткий меч. На его эфесе выгравированы слова: УДАЧИ! МЕРЛИН.",
+            place: 9,
+        }, {
+            index: 3,
+            name: "шест",
+            case: "шест",
+            desc: "Это шест, который остался от разломанной мною лестницы.",
+            place: -1
         }],
 
         // Флаги и триггеры (true или false, как правило)
         flags: {
-
+            isLadderLeanToTree: false
         }
     };
 
